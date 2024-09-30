19ab9af188ff5be058e649d2e351e176db39bb72
RyuuyaS <thinhyolo1994@gmail.com>
2024-09-30 21:55:07 +0700

    Init lint and format

--- components/Maps/MapOne.tsx
+++ components/Maps/MapOne.tsx
@@ -1,61 +1,61 @@
-"use client";
-import jsVectorMap from "jsvectormap";
-import "jsvectormap/dist/jsvectormap.css";
-import React, { useEffect } from "react";
-import "../../js/us-aea-en";
+'use client';
+import jsVectorMap from 'jsvectormap';
+import 'jsvectormap/dist/jsvectormap.css';
+import React, { useEffect } from 'react';
+import '../../js/us-aea-en';
 
 const MapOne: React.FC = () => {
   useEffect(() => {
     const mapOne = new jsVectorMap({
-      selector: "#mapOne",
-      map: "us_aea_en",
+      selector: '#mapOne',
+      map: 'us_aea_en',
       zoomButtons: true,
 
       regionStyle: {
         initial: {
-          fill: "#C8D0D8",
+          fill: '#C8D0D8',
         },
         hover: {
           fillOpacity: 1,
-          fill: "#3056D3",
+          fill: '#3056D3',
         },
       },
       regionLabelStyle: {
         initial: {
-          fontFamily: "Satoshi",
-          fontWeight: "semibold",
-          fill: "#fff",
+          fontFamily: 'Satoshi',
+          fontWeight: 'semibold',
+          fill: '#fff',
         },
         hover: {
-          cursor: "pointer",
+          cursor: 'pointer',
         },
       },
 
       labels: {
         regions: {
           render(code: string) {
-            return code.split("-")[1];
+            return code.split('-')[1];
           },
         },
       },
     });
 
     return () => {
-      const map = document.getElementById("mapOne");
+      const map = document.getElementById('mapOne');
       if (map) {
-        map.innerHTML = "";
+        map.innerHTML = '';
       }
       // mapOne.destroy();
     };
   }, []);
 
   return (
-    <div className="col-span-12 rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-7">
-      <h4 className="mb-2 text-xl font-semibold text-black dark:text-white">
+    <div className='col-span-12 rounded-sm border border-stroke bg-white px-7.5 py-6 shadow-default xl:col-span-7 dark:border-strokedark dark:bg-boxdark'>
+      <h4 className='mb-2 text-xl font-semibold text-black dark:text-white'>
         Region labels
       </h4>
-      <div className="h-90">
-        <div id="mapOne" className="mapOne map-btn"></div>
+      <div className='h-90'>
+        <div id='mapOne' className='mapOne map-btn'></div>
       </div>
     </div>
   );
