3344a77439c28bc82ee78ad506b2e5de9776ccd7
toanbdhe161559 <buiductoanfu@gmail.com>
2024-10-15 01:03:41 +0700

    task: 03 Update validation, add cancel button

--- lib/schemaValidate/branchSchema.ts
+++ lib/schemaValidate/branchSchema.ts
@@ -1,14 +1,28 @@
 import z from "zod";
 
+// Biểu thức chính quy cho số điện thoại Việt Nam
+const phoneNumberRegex = /^(0(1[0-9]|2[0-9]|3[0-9]|4[0-9]|5[0-9]|6[0-9]|7[0-9]|8[0-9]|9[0-9])\d{7}|(0[2-9]\d{7}))$/;
+
 export const BranchBody = z
     .object({
-        branchName: z.string().trim().min(1, "Vui lòng nhập tên chi nhánh").max(256, "Giới hạn 255 kí tự"),
+        branchName: z.string().trim().min(1, "Vui lòng nhập tên chi nhánh").max(100, "Giới hạn 100 kí tự"),
         location: z.string().trim().min(1, "Vui lòng nhập địa chỉ chi nhánh").max(256, "Giới hạn 255 kí tự"),
-        contactPerson: z.string().trim().max(256, "Giới hạn 255 kí tự"),
-        phoneNumber: z.string().trim().min(1, "Vui lòng nhập số điện thoại liên hệ").max(50, "Giới hạn 50 kí tự"),
-        branchType: z.coerce.string({ required_error: "Vui lòng chọn kiểu chi nhánh" }),
-        capacity: z.coerce.number({ message: "Vui lòng nhập số" }).int("Vui lòng nhập số nguyên"),
+        contactPerson: z.string().trim().max(100, "Giới hạn 100 kí tự").optional(),
+        phoneNumber: z
+            .string()
+            .trim()
+            .min(1, "Vui lòng nhập số điện thoại liên hệ")
+            .max(11, "Giới hạn 11 kí tự")
+            .regex(phoneNumberRegex, "Số điện thoại không đúng định dạng"), // Kiểm tra định dạng,
+        capacity: z.coerce.number({ message: "Vui lòng nhập số" }).int("Vui lòng nhập số nguyên").optional(),
         activeStatus: z.boolean(),
+        // Use preprocess to handle null/undefined
+        branchType: z.preprocess(
+            (val) => (val === null || val === undefined || val === "" ? undefined : val), // Turn null to undefined
+            z.enum(["MAIN", "SUB"], {
+                required_error: "Vui lòng chọn kiểu chi nhánh",
+            })
+        ),
     })
     .strict();
 
@@ -20,13 +34,13 @@ export const BranchRes = z.object({
         status: z.string(),
         data: z.object({
             id: z.number(),
-            branchName: z.string().trim().min(2).max(256),
+            branchName: z.string().trim().min(1).max(100),
             location: z.string().trim().max(256),
-            contactPerson: z.string().trim().max(256),
-            phoneNumber: z.string().trim().min(6).max(50),
-            branchType: z.string(),
+            contactPerson: z.string().trim().max(100),
+            phoneNumber: z.string().trim().min(1).max(11),
             capacity: z.number(),
             activeStatus: z.boolean(),
+            branchType: z.enum(["MAIN", "SUB"]),
         }),
     }),
     message: z.string(),
