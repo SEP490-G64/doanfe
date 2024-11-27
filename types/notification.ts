37de4f13288e618839e8b770fee63f18c07459e9
toanbdhe161559 <buiductoanfu@gmail.com>
2024-11-27 09:20:31 +0700

    task: 09 notifications redirect

--- types/notification.ts
+++ types/notification.ts
@@ -2,4 +2,5 @@ export interface Notification {
     id: number;
     notification: { id: number; notiType: string; notiName: string; message: string };
     read: boolean;
+    createdDate: string | Date;
 }
